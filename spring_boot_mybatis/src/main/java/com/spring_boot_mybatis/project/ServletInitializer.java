d2df0cf9b577bd9e1405eb8ffa30fc70ce2bd7ff
bomooline <bomooline@naver.com>
2024-09-04 00:59:35 +0900

    boot

--- spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/ServletInitializer.java
+++ spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/ServletInitializer.java
@@ -1,12 +1,18 @@
 package com.spring_boot_mybatis.project;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootApplication
-public class ServletInitializer extends SpringBootServletInitializer {
-
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer{
+	
+	@Override
+	protected WebApplicationContext run(SpringApplication application) {
+		return super.run(application);
+	}
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootMybatisApplication.class);
